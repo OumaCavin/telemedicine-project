18c04a6f98b564e09f071c10a1c331ae50e0b1da
OumaCavin <cavin.otieno012@gmail.com>
2026-04-05 02:49:50 +0800

    fix(backend): fix undefined methods in chatRoutes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,10 +5,10 @@ const MessageController = require('../controllers/MessageController');
 const { protect } = require('../middlewares/authMiddleware');
 
 // Route to get all messages in a chat
-router.get('/:chatId', protect, MessageController.getChatMessages);
+router.get('/:chatId', protect, MessageController.searchMessages);
 
 // Route to send a new message in a chat
-router.post('/:chatId', protect, MessageController.sendMessage);
+router.post('/:chatId', protect, MessageController.createMessage);
 
 // Route to delete a message in a chat
 router.delete('/:chatId/:messageId', protect, MessageController.deleteMessage);
